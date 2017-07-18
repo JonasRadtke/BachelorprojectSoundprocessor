f33b00306d99feb9f2f4937506a4a5a258dcb64e
JonasRadtke <jonas@radtke.dk>
2017-07-18 18:22:21 +0200

    alles wieder da, gitignore angepasst

---
+++ Soundprozessor/src/main.c
@@ -0,0 +1,116 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+#include "main.h"
+
+
+
+// Externe Globale Variablen
+extern volatile chan channel[8];
+extern union dds_cnt_t dds_counter[8];
+
+extern volatile uint16_t noten;
+extern volatile uint8_t dreiecktabelle;
+
+extern uint8_t tasten[2];
+
+
+
+volatile uint32_t ticks = 0; // Systemzeit seit Start in Millisekunden
+uint32_t wt = 0;
+
+
+
+int main (void)
+{
+	
+	sysclk_init();		// 48 Mhz einstellen
+	wdt_disable(WDT);	// Watchdog ausschalten
+	SystemCoreClockUpdate();	// Systemclock akualisieren
+	fpu_enable ();	// Floatingpoint Unit aktivieren, Achtung richtige Optimierung einstellen!
+	delay_init(SystemCoreClock);	
+	SystemCoreClock = 44300000;
+	SysTick_Config(SystemCoreClock / 1000);      /* Configure SysTick to generate an interrupt every millisecond */
+
+	pmc_enable_periph_clk(ID_PIOA);	
+	pmc_enable_periph_clk(ID_PIOB);
+	pio_set_output 	( 	PIOA, D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7 | DAC_NWRITE ,LOW,DISABLE,DISABLE); // Setze Ausgänge
+	
+	// Timer initalisieren! 
+	timerInit();
+
+	//TWI I2C Portexpander initalisieren
+	twiInit();
+
+	//UART initalisieren
+	uartInit();
+
+
+	//ADC Init
+//	pmc_enable_periph_clk(ADC);
+//	adc_enable();
+//	adc_get_config_defaults(&adc_cfg);
+//	adc_init(ADC, &adc_cfg);
+//	adc_set_trigger(ADC, ADC_TRIG_SW);
+//	adc_channel_enable(ADC, ADC_CHANNEL_2);
+
+	uint32_t delaytemp = 0;
+	//delay_ms(1000);
+	dac_out = 0;
+	
+	// ZUM OSZILLATOR TESTEN!
+	channel[0].oscillator_on = 1;
+	channel[0].waveform = TRIANGLE; // RECTANGLE/TRIANGLE / NOISE
+	channel[0].frequency = 880;
+	//
+	
+	uint8_t settings = 0;	//Einstellen der Modi auf Standardwerte
+	
+	while(1)
+	{
+		
+		
+
+	//	portexpander_einlesen(tasten[0]);
+
+	char text[200];
+	sprintf(text,   " Tasten 1: \n");
+	uartsendstring(text);
+	
+	delaytemp = ticks;
+	
+	while (!((ticks) >= delaytemp+1000));
+
+	
+	//	print_tasten();
+		// Der Phasenakkumulator ist 32 bit, die höchsten 8 bit werden zum springen der Tabelle benutzt, 24bit sind quasi Nachkommastellen.
+		// In einer eigenen ISR, vermutlich 100khz, werden zu den 24bit immer die Stepsize dazu addiert. Diese wird einfach vorberechnet. 
+		// Bei jeden Überlauf der 24bit erhöht sich dadurch der Index der Tabelle. 0000 0001 + 24bit. Durch anpassen der Stepsize geschieht dies schneller oder langsamer.
+		channel[0].rect_end = SAMPLEFREQ / channel[0].frequency;
+		channel[0].rect_low = channel[0].rect_end / 2 ;
+		channel[0].tri_stepsize = (BIT24*channel[0].frequency*TRITAB)/PHASEAKKU_FREQ; //Zaehler für Phasenakkumulator
+	}
+}
+
+//
+
+
+// Systemtakt 1ms
+void SysTick_Handler(){
+	ticks++;	
+}
+
+
+void print_tasten()
+{
+	char text[200];
+	sprintf(text,   " Tasten 1: %d  \n", tasten[0]);
+	uartsendstring(text);
+	sprintf(text,	" Tasten 2: %d  \n\n", tasten[1]);
+	uartsendstring(text);
+	return;
+}
---
+++ Soundprozessor/src/oszillatoren.h
@@ -0,0 +1,92 @@
+/*
+ * oszillatoren.h
+ *
+ * Created: 23.05.2017 12:30:14
+ *  Author: Radtke
+ */ 
+
+
+#ifndef OSZILLATOREN_H_
+#define OSZILLATOREN_H_
+
+#include <asf.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+#define SAMPLEFREQ			(38000)
+#define PHASEAKKU_FREQ		(100000)
+#define DAC_NWRITE			(PIO_PA4) 
+#define D0					(PIO_PA5)
+#define D1				    (PIO_PA6)
+#define D2					(PIO_PA7)
+#define D3					(PIO_PA8)
+#define D4					(PIO_PA9)
+#define D5					(PIO_PA10)
+#define D6					(PIO_PA11)
+#define D7					(PIO_PA12)
+#define TRITAB				(16)
+#define BIT24				(16777216)
+
+#define RECTANGLE			(1)
+#define TRIANGLE			(2)
+#define NOISE				(3)
+
+volatile uint8_t dac_out;
+
+union dds_cnt_t{
+	struct{
+		uint8_t byte1;
+		uint8_t byte2;
+		uint8_t byte3;
+		uint8_t phase;
+	};
+	uint32_t counter;
+} ;
+
+typedef struct chan1 {
+	uint8_t oscillator_on;	// Chanel On/off
+	uint8_t waveform;		// Waveform, RECTANGLE, TRIANGLE, NOISE
+	uint8_t pushed_key;		// Number of pushed Key in this Oscillator
+	float frequency;		// Frequency of Note
+	
+	uint8_t dutycycle;       // Duty Cycle in percent 0-100%
+	uint16_t rect_low;	   	// Rectangle, this is the Dutycycle 
+	uint16_t rect_end;		// Rectangle, end
+	uint16_t rect_count;	 // Rectangle counter, for internal counting
+	
+	uint8_t tri_table_index; // Indexing the Table of Triangle
+	uint32_t tri_stepsize;	 // Vorberechnete Schrittweite
+	
+	union dds_cnt_t dds_counter;
+	
+	uint8_t chan_out;		// Output of Chanel
+	
+	//struct unpacker		trackup;
+	//u8			tnum;
+	//s8			transp;
+	//u8			tnote;
+	//u8			lastinstr;
+	//u8			inum;
+	//u16			iptr;
+	//u8			iwait;
+	//u8			inote;
+	//s8			bendd;
+	//s16			bend;
+	//s8			volumed;
+	//s16			dutyd;
+	//u8			vdepth;
+	//u8			vrate;
+	//u8			vpos;
+	//s16			inertia;
+	//u16			slur;
+} chan;
+
+
+
+void timerInit (void);
+void oscillator(chan *);
+
+
+#endif /* OSZILLATOREN_H_ */
